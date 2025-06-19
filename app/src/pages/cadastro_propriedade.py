c2b0ec762d02469edb77286b89e99421b20ab36d
Brenno Oliveria <118616498+Ubrennz@users.noreply.github.com>
2025-06-19 17:57:15 -0300

    Adicionados novos TextFields e CheckBoxes

--- app/src/pages/cadastro_propriedade.py
+++ app/src/pages/cadastro_propriedade.py
@@ -6,6 +6,9 @@ def cadastroPropriedade(page: ft.Page):
 
     tb.topBar(page)
 
+    page.scroll = ft.ScrollMode.ALWAYS
+    page.update()
+
     page.add(
         ft.Text(
             "Cadastro do Propriedade",
@@ -17,6 +20,13 @@ def cadastroPropriedade(page: ft.Page):
 
     def button_clicked(e):
         try:
+            sistema_criacao = (
+                "Confinamento" if sistema_criacao_confinamento.value else
+                "Semi-confinado" if sistema_criacao_semi_confinado.value else
+                "Apasto" if sistema_criacao_apasto.value else
+                "Não informado"
+            )
+
             t.value = f'''Textboxes values are:
             '{nome_propriedade.value}',
             '{nome_proprietario.value}', 
@@ -25,18 +35,43 @@ def cadastroPropriedade(page: ft.Page):
             '{int(numero_piquetes.value)}'
             '{int(numero_curais.value)}'
             '{int(qtde_funcionarios.value)}'
-            '{'Sim' if possui_maquinas_sim.value else 'Não' if possui_maquinas_nao.value else 'Não informado'}
-            '{quais_maquinas.value if possui_maquinas_sim else None}'.'''
+            '{'Sim' if possui_maquinas_sim.value else 'Não' if possui_maquinas_nao.value else 'Não informado'}'
+            '{quais_maquinas.value}'
+            '{int(qtde_total_animais.value)}'
+            '{raca_predominante.value}'
+            '{sistema_criacao}'
+            '{'Sim' if usa_racao_sim.value else 'Não' if usa_racao_nao.value else 'Não informado'}'.'''
         except ValueError:
             t.value = "Preencha todos os campos corretamente! (Atenção aos campos numéricos)"
         
         page.update()
 
-    def checkbox_changed(e):
+    def checkbox_possui_maquinas(e):
         if e.control == possui_maquinas_sim and possui_maquinas_sim.value:
             possui_maquinas_nao.value = False
         elif e.control == possui_maquinas_nao and possui_maquinas_nao.value:
             possui_maquinas_sim.value = False
+
+        page.update()
+
+    def checkbox_sistema_criacao(e):
+        if e.control == sistema_criacao_confinamento and sistema_criacao_confinamento.value:
+            sistema_criacao_semi_confinado.value = False
+            sistema_criacao_apasto.value = False
+        elif e.control == sistema_criacao_semi_confinado and sistema_criacao_semi_confinado.value:
+            sistema_criacao_confinamento.value = False
+            sistema_criacao_apasto.value = False
+        elif e.control == sistema_criacao_apasto and sistema_criacao_apasto.value:
+            sistema_criacao_confinamento.value = False
+            sistema_criacao_semi_confinado.value = False
+
+        page.update()
+
+    def checkbox_usa_racao(e):
+        if e.control == usa_racao_sim and usa_racao_sim.value:
+            usa_racao_nao.value = False
+        elif e.control == usa_racao_nao and usa_racao_nao.value:
+            usa_racao_sim.value = False
 
         page.update()
 
@@ -52,6 +87,10 @@ def cadastroPropriedade(page: ft.Page):
         "text_style": ft.TextStyle(weight=ft.FontWeight.BOLD),     
     }
 
+    estilo_label = {
+        "label_style": ft.TextStyle(size=18, weight=ft.FontWeight.BOLD)
+    }
+
     nome_propriedade = ft.TextField(hint_text="Nome da Propriedade", **estilo_padrao)
     nome_proprietario = ft.TextField(hint_text="Nome do Proprietario", **estilo_padrao)
     endereco_propriedade = ft.TextField(hint_text="Endereço da Propriedade", **estilo_padrao)
@@ -60,10 +99,10 @@ def cadastroPropriedade(page: ft.Page):
     numero_curais = ft.TextField(hint_text="Número de Curais", **estilo_padrao, keyboard_type=ft.KeyboardType.NUMBER)
     qtde_funcionarios = ft.TextField(hint_text="Quantidade de Funcionários", **estilo_padrao, keyboard_type=ft.KeyboardType.NUMBER)
         
-    possui_maquinas_sim = ft.Checkbox(label="Sim", label_style=ft.TextStyle(size=18, weight=ft.FontWeight.BOLD))
-    possui_maquinas_nao = ft.Checkbox(label="Não", label_style=ft.TextStyle(size=18, weight=ft.FontWeight.BOLD))
-    possui_maquinas_sim.on_change = checkbox_changed
-    possui_maquinas_nao.on_change = checkbox_changed
+    possui_maquinas_sim = ft.Checkbox(label="Sim", **estilo_label)
+    possui_maquinas_nao = ft.Checkbox(label="Não", **estilo_label)
+    possui_maquinas_sim.on_change = checkbox_possui_maquinas
+    possui_maquinas_nao.on_change = checkbox_possui_maquinas
 
     possui_maquinas_container = ft.Container(
         content=ft.Column([
@@ -77,8 +116,46 @@ def cadastroPropriedade(page: ft.Page):
         padding=10,        
     )
 
-    if possui_maquinas_sim:
-        quais_maquinas = ft.TextField(hint_text="Quais Maquinas?", **estilo_padrao)
+    quais_maquinas = ft.TextField(hint_text="Quais maquinas?", **estilo_padrao)
+    qtde_total_animais = ft.TextField(hint_text="Quantidade Total de Animais", **estilo_padrao, keyboard_type=ft.KeyboardType.NUMBER)
+    raca_predominante = ft.TextField(hint_text="Raça Predominante", **estilo_padrao)
+
+    sistema_criacao_confinamento = ft.Checkbox(label="Confinamento", **estilo_label)
+    sistema_criacao_semi_confinado = ft.Checkbox(label="Semi-confinado", **estilo_label)
+    sistema_criacao_apasto = ft.Checkbox(label="Apasto", **estilo_label)
+    sistema_criacao_confinamento.on_change = checkbox_sistema_criacao
+    sistema_criacao_semi_confinado.on_change = checkbox_sistema_criacao
+    sistema_criacao_apasto.on_change = checkbox_sistema_criacao
+
+    sistema_criacao_container = ft.Container(
+        content=ft.Column([
+            ft.Text("Sistema de Criacão", weight="bold", size=18),
+            sistema_criacao_confinamento,
+            sistema_criacao_semi_confinado,
+            sistema_criacao_apasto,
+        ]),        
+        bgcolor=ft.Colors.WHITE,
+        border=ft.border.all(1, ft.Colors.GREY),
+        border_radius=8,
+        padding=10,        
+    )
+
+    usa_racao_sim = ft.Checkbox(label="Sim", **estilo_label)
+    usa_racao_nao = ft.Checkbox(label="Não", **estilo_label)
+    usa_racao_sim.on_change = checkbox_usa_racao
+    usa_racao_nao.on_change = checkbox_usa_racao
+
+    usa_racao_container = ft.Container(
+        content=ft.Column([
+            ft.Text("Usa ração?", weight="bold", size=18),
+            usa_racao_sim,
+            usa_racao_nao,            
+        ]),        
+        bgcolor=ft.Colors.WHITE,
+        border=ft.border.all(1, ft.Colors.GREY),
+        border_radius=8,
+        padding=10,        
+    )
 
     botao = ft.ElevatedButton(
         text="Cadastrar",
@@ -102,13 +179,16 @@ def cadastroPropriedade(page: ft.Page):
                 numero_piquetes,
                 numero_curais,
                 qtde_funcionarios,
-                possui_maquinas_container,                
+                possui_maquinas_container,
+                quais_maquinas,
+                qtde_total_animais,
+                raca_predominante,
+                sistema_criacao_container,
+                usa_racao_container,                  
                 botao,
                 t
             ],
             horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-            spacing=15
+            spacing=15            
         )
-    )
-
-    
+    )    
