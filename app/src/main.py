736c4fc80a97d13b52ad02a134febb9d57803058
Brenno Oliveria <118616498+Ubrennz@users.noreply.github.com>
2025-06-22 18:49:13 -0300

    alterações nos chamados das paginas

--- app/src/main.py
+++ app/src/main.py
@@ -1,9 +1,11 @@
 import flet as ft
-# from pages import cadastro_produtor as produtor
+from pages import cadastro_produtor as produtor
 from pages import cadastro_tecnico as tecnico
 from pages import cadastro_propriedade as propriedade
 
 def main(page: ft.Page):
     propriedade.cadastroPropriedade(page)
+    # tecnico.cadastroTecnico(page)
+    # produtor.cadastroProdutor(page)
 
 ft.app(target=main, port=8551, host="0.0.0.0" )
