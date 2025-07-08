e56b3b63360b1b2ab1ee5c06dcfcf24bc1b9927c
Brenno Oliveria <118616498+Ubrennz@users.noreply.github.com>
2025-07-08 14:09:32 -0300

    mudança na chamada do banco de dados

--- app/src/main.py
+++ app/src/main.py
@@ -1,10 +1,10 @@
 import flet as ft
-from database import criar_tabelas as tabelas
+from database.init_db import initDB
 from pages import primeira_tela as primeira
 
 def main(page: ft.Page):    
     primeira.primeiraTela(page)
 
 if __name__ == "__main__":
-    tabelas.criar_tabela() 
+    initDB()
     ft.app(target=main, view=ft.WEB_BROWSER)
